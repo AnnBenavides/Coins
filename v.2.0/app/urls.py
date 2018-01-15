bd5d93411842a59559b2301c7e655566a2386117
AnnBenavides <andrea.benavidesj@gmail.com>
2018-01-14 21:09:06 -0300

    Vista perfil-profe mockup

--- v.2.0/app/urls.py
+++ v.2.0/app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
         url(r'^$', views.users_list),
+        url(r'^profe/(?P<pk>[0-9]+)/$', views.perfil_profe, name='perfil_profe'),
     ]
--- v.2.0/app/views.py
+++ v.2.0/app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Alumno, Taller, Grupo, Profesor
 
 # Create your views here.
@@ -6,3 +6,7 @@ from .models import Alumno, Taller, Grupo, Profesor
 def users_list(request):
 	profesores = Profesor.objects.all()
 	return render(request, 'app/users_list.html', {'profesores' : profesores})
+
+def perfil_profe(request,pk):
+	profe = get_object_or_404(Profesor, pk=pk)
+	return render(request, 'app/perfil_profe.html', {'profe' : profe})
